a316bb649e192c43cfd0023d34ef3f1aea645c92
Lindsay Warr <iamalion@gmail.com>
2023-09-13 11:48:56 -0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Lindsay Warr Portfolio</title>
+                <meta name="description" content="Portfolio" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <section className="min-h-screen">
+                    <h1>Hello</h1>
+
+                </section>
+            </main>
+        </div>
+    )
+}
